55b38c567e9f7f21890f919a8d2a78fab184aa25
rglz21 <68825014+rglz21@users.noreply.github.com>
2026-06-06 20:59:42 -0600

    Tarea
    Ejercicio terminado

---
+++ Calculadora_Basica/Form1.Designer.cs
@@ -0,0 +1,167 @@
+﻿namespace Calculadora_Basica
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtnum1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtnum2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.ErrorEncontrado = new System.Windows.Forms.ErrorProvider(this.components);
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.ErrorEncontrado)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(78, 78);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(54, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "1- Nunero";
+            // 
+            // txtnum1
+            // 
+            this.txtnum1.Location = new System.Drawing.Point(81, 94);
+            this.txtnum1.Name = "txtnum1";
+            this.txtnum1.Size = new System.Drawing.Size(261, 20);
+            this.txtnum1.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(81, 131);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(56, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "2- Numero";
+            // 
+            // txtnum2
+            // 
+            this.txtnum2.Location = new System.Drawing.Point(84, 148);
+            this.txtnum2.Name = "txtnum2";
+            this.txtnum2.Size = new System.Drawing.Size(258, 20);
+            this.txtnum2.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(482, 66);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(105, 30);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "SUMAR";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(482, 102);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(105, 23);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "RESTAR";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(482, 132);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(105, 29);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "MULTIPLICAR";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(482, 168);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(105, 23);
+            this.button4.TabIndex = 7;
+            this.button4.Text = "DIVIDIR";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // ErrorEncontrado
+            // 
+            this.ErrorEncontrado.ContainerControl = this;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Noto Sans Armenian", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(123, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(442, 51);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "CALCULADORA BASICA";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(682, 230);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtnum2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtnum1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.ErrorEncontrado)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtnum1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtnum2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.ErrorProvider ErrorEncontrado;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Calculadora_Basica/Form1.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculadora_Basica
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private bool verificar_Contenido()
+        {
+
+            bool valido = true;
+
+            ErrorEncontrado.Clear();
+
+            if (string.IsNullOrWhiteSpace(txtnum1.Text))
+            {
+                ErrorEncontrado.SetError(txtnum1, "Ingrese el primer número.");
+                valido = false;
+            }
+            else if (!double.TryParse(txtnum1.Text, out _))
+            {
+                ErrorEncontrado.SetError(txtnum1, "Debe ingresar un valor numérico.");
+                valido = false;
+            }
+
+            if (string.IsNullOrWhiteSpace(txtnum2.Text))
+            {
+                ErrorEncontrado.SetError(txtnum2, "Ingrese el segundo número.");
+                valido = false;
+            }
+            else if (!double.TryParse(txtnum2.Text, out _))
+            {
+                ErrorEncontrado.SetError(txtnum2, "Debe ingresar un valor numérico.");
+                valido = false;
+            }
+
+            return valido;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (!verificar_Contenido())
+                return;
+
+            double num1 = double.Parse(txtnum1.Text);
+            double num2 = double.Parse(txtnum2.Text);
+
+            double resultado = num1 + num2;
+
+            MessageBox.Show("Resultado: " + resultado, "RESPUESTA", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (!verificar_Contenido())
+                return;
+
+            double num1 = double.Parse(txtnum1.Text);
+            double num2 = double.Parse(txtnum2.Text);
+
+            double resultado = num1 - num2;
+
+            MessageBox.Show("Resultado: " + resultado, "RESPUESTA", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (!verificar_Contenido())
+                return;
+
+            double num1 = double.Parse(txtnum1.Text);
+            double num2 = double.Parse(txtnum2.Text);
+            if (num1 == 0 && num2==0)
+            {
+                MessageBox.Show("No se puede multiplicar cero por cero.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            double resultado = num1 * num2;
+
+            MessageBox.Show("Resultado: " + resultado, "RESPUESTA", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (!verificar_Contenido())
+                return;
+
+            double num1 = double.Parse(txtnum1.Text);
+            double num2 = double.Parse(txtnum2.Text);
+            if (num2 == 0)
+            {
+                MessageBox.Show("No se puede dividir entre cero.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            double resultado = num1 / num2;
+
+            MessageBox.Show("Resultado: " + resultado, "RESPUESTA", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+    }
+}
